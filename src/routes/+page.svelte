0de5a56ff9dc8f754e2a838def73d010e8a52443
execphantasmagoria <deadmandieddeathless@gmail.com>
2026-03-03 16:55:35 +0530

    UPDATED: minor tweaks to test homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,8 @@ import { getPublicFileUrl } from '$lib/storage'
   const bgUrl = getPublicFileUrl('portfolio-assets', 'bg/bg1.png')
 </script>
 
+<h1>My Portfolio (to be implemented)</h1>
+
 <ul>
   {#each data.instruments as instrument}
     <li>{instrument.name}</li>
